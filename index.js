f1888255bcbbf0bafbccd34c1e1a1427854b63ee
Guilherme Kaercher <contato@guilhermekaercher.com.br>
2019-02-05 13:36:54 -0200

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 
-// process.env.AWS_ACCESS_KEY_ID = 'AKIAJ5SKZZTOCJOFUB3A'
-// process.env.AWS_SECRET_ACCESS_KEY = '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'
-
 app.get('/', function (req, res) {
 
   res.send('Hello World!')
